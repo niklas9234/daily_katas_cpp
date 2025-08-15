c001fddb18523ab83c24f9fcfb2c7a7e6dfdd87b
Niklas Conrad <conradurnshausen@gmail.com>
2025-08-15 12:15:53 +0200

    Advent of code - 1

--- kw_33/main.cpp
+++ kw_33/main.cpp
@@ -1,6 +1,21 @@
+#include <algorithm>
 #include <iostream>
+#include <array>
+
+// Advent of code - 1
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::array l = {3,4,2,1,3,3};
+    std::array r = {4,3,5,3,9,3};
+
+    std::sort(l.begin(), l.end());
+    std::sort(r.begin(), r.end());
+
+    int sum {};
+    for (int i = 0; i<l.size(); i++) {
+        sum += std::abs(l[i]-r[i]);
+    }
+
+    std::cout << sum;
     return 0;
 }
